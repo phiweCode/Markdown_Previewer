9332e250b0ccd34d81d2119e2e260f3e06c105bf
phiweCode <thabo.s.mngoma@gmail.com>
2023-11-16 14:00:46 +0200

    some changes

--- Markdown-Previewer/src/main.jsx
+++ Markdown-Previewer/src/main.jsx
@@ -6,6 +6,7 @@ import './scss/style.scss'
 import * as boostrap from 'bootstrap'
 import { registerLicense } from '@syncfusion/ej2-base';
 
+registerLicense('Ngo9BigBOggjHTQxAR8/V1NHaF5cXmVCf1FpRmJGdld5fUVHYVZUTXxaS00DNHVRdkdgWH5fcnVVQ2JfUk10WkI=');
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
