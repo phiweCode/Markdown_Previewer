d133407023b50e8623d0c7066e22e600ce370cb9
phiweCode <thabo.s.mngoma@gmail.com>
2023-11-20 14:41:12 +0200

    test deploy

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,8 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom'
 import App from './App.jsx'
 import './index.css'
 import './scss/style.scss'
-import * as boostrap from 'bootstrap'
 
 import { registerLicense } from '@syncfusion/ej2-base';
 
